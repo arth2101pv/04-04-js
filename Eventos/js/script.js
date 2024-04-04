254d992e3988ed216c2b497e13dd86ce71b7e447
ARTHUR DOS SANTOS CABRAL <arthur.scabral@senacsp.edu.br>
2024-04-04 14:15:22 -0300

    Atualização

--- Eventos/js/script.js
+++ Eventos/js/script.js
@@ -21,3 +21,52 @@ thirdBtn.addEventListener("click", () => {
         console.log("Clicou!")
     })
 })
+
+const title = document.querySelector("#my-title");
+
+title.addEventListener("click", (event) =>{
+    // console.log(event)
+    console.log(e.offsetX);
+})
+
+const containerBtn = document.querySelector("#btn-container")
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+  console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+    console.log("Evento 2");
+    e.stopPropagation();
+});
+
+const a = document.querySelector("a");
+
+a.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Não alterou a página");
+})
+
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`)
+})
+document.addEventListener("keydown", (e) => {
+    console.log(`Pressionou a tecla ${e.key}`);
+});
+
+const mouseEvents = document.querySelector("#mouse");
+mouseEvents.addEventListener("mousedown", () => {
+    console.log("Pressionou o botão");
+});
+mouseEvents.addEventListener("mouseup", () => {
+    console.log("Soltou o botão");
+});
+mouseEvents.addEventListener("mouseup", () => {
+    console.log("Clique duplo");
+});
+window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 200){
+        console.log("Passamos 200px!");
+    }
+})
